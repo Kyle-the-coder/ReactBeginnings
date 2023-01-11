5430e883e197e405032848b375b6a4acb8374bac
Kyle Mitchell <111798115+Kyle-the-coder@users.noreply.github.com>
2023-01-11 15:39:06 -0800

    Add files via upload

--- prop-it-up/src/components/person.js
+++ prop-it-up/src/components/person.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NewPerson extends Component {
-
-    constructor(props){
-        super(props);
-        this.state={
-            age: this.props.age
-        }
-
-    }
-
-    birthday = () => {
-        
-        this.setState({age: this.state.age + 1})
-    }
-    render(){
+const NewPerson = props => {
+    
         return(
             <div>
                 <h1>
-                {this.props.lastName}, {this.props.firstName}
+                {props.lastName}, {props.firstName}
                 </h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={this.birthday}>It's {this.props.firstName} Birthday! Age+1</button>
+                <p>Age: {props.age}</p>
+                <p>Hair Color: {props.hairColor}</p>
+                <button >It's {props.firstName} Birthday! Age+1</button>
             </div>
         )
-    }
+    
 }
 
 export default NewPerson;
