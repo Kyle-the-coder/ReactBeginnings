4dbe7d56be8ee07994b1234ed91fbebdae69b899
Kyle Mitchell <111798115+Kyle-the-coder@users.noreply.github.com>
2023-01-10 18:13:54 -0800

    Add files via upload

---
+++ tj/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo!</h1>
+      <h1> Things I need to do:</h1>
+      <p>*Learn React</p>
+      <p>*Climb Mt. Everest</p>
+      <p>*Run a marathon</p>
+      <p>*Feed the dogs</p>
+
+      
+    </div>
+  );
+}
+
+export default App;
